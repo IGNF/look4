17a529a7a092be8dfc0fe116e69284cace529ed8
Romain Wieser <romainwieser@gmail.com>
2017-12-04 17:09:41 +0100

    API doc 1.5.0 import

---
+++ 1.0.5/jsdoc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "look4",
+  "version": "1.5.0",
+  "description": "Search engines aggregtor",
+  "sampleUrle": "http://127.0.0.1:8888",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2017-12-04T16:06:06.899Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
